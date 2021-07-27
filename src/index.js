210392209b780d6e27319cf9ed79704613a8d40b
601hacker <farhanishraq601@gmail.com>
2021-07-27 19:58:56 +0600

    Added buttons

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-import { newvar } from './home';
-console.log(newvar+ ' kajdpoajfpa');
+let body = document.querySelector('body');
+
+const createButtons = (function() {
+        let homeButton = function() {
+            let homeButton = document.createElement('button');
+            homeButton.innerText = 'Home';
+            body.appendChild(homeButton);
+        }
+
+        let menuButton = function() {
+            let menuButton = document.createElement('button');
+            menuButton.innerText = 'menu';
+            body.appendChild(menuButton);
+        }
+        let contactButton = function() {
+            let contactButton = document.createElement('button');
+            contactButton.innerText = 'Contact';
+            body.appendChild(contactButton);
+        }
+
+        return {
+            createAllButtons: function() {
+                homeButton();
+                menuButton();
+                contactButton();
+            }
+        }
+})();
+
+createButtons.createAllButtons();
